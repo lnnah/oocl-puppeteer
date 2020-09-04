e8bfebde4cf73353287bbbbeb1a9b658451abc9f
lnnah <nam.le@dounets.com>
2020-09-04 11:24:54 +0700

    Convert to chrome-aws-lambda

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+'use strict'
+
 const OOCLService = require('./oocl-service')
 
-try {
-  const ooclService = new OOCLService(['OOLU0571619'])
-  ooclService.run()
-} catch (error) {
-  console.log(error)
-}
+// module.exports.index = async (event, context) => {
+  try {
+    const ooclService = new OOCLService(['OOLU0571619', 'FCIU5229601'])
+    ooclService.run()
+  } catch (error) {
+    console.log(error)
+  }
+// }
+
--- oocl-service.js
+++ oocl-service.js
@@ -1,14 +1,10 @@
-const puppeteer = require('puppeteer')
+const chromium = require('chrome-aws-lambda')
+const puppeteer = chromium.puppeteer
 
 class OOCLService {
   SERVICE_NAME = 'OoclService'
   MAX_RETRY_TIME = 1
   SLEEP_TIME = 10
-
-  defaultSecondPageParams = {
-    'hiddenForm:_link_hidden_': 'hiddenForm:goToCargoTrackingCNTR',
-    'hiddenForm:supportUtfChars': true
-  }
 
   currentIndex = -1
 
@@ -17,9 +13,6 @@ class OOCLService {
   USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.137 Safari/537.36'
   
   MAIN_URL = 'https://www.oocl.com/eng/ourservices/eservices/cargotracking/Pages/cargotracking.aspx'
-  SECOND_URL = 'http://moc.oocl.com/party/cargotracking/ct_search_from_other_domain.jsf?ANONYMOUS_BEHAVIOR=BUILD_UP&domainName=PARTY_DOMAIN&ENTRY_TYPE=OOCL&ENTRY=MCC&ctSearchType=CNTR&ctShipmentNumber=%{con}'
-  THIRD_URL = 'http://moc.oocl.com/party/cargotracking/ct_search_from_other_domain.jsf?ANONYMOUS_TOKEN=%{userToken}&ENTRY=MCC&ENTRY_TYPE=OOCL&PREFER_LANGUAGE=en-US'
-
 
   constructor(containers) {
     this.containers = containers
@@ -28,15 +21,16 @@ class OOCLService {
   run = async () => {
     for (let container of this.containers) {
       this.currentIndex++
-      this.nextContainer(container)
+      this.currentContainerNo = container
       
       const browser = await puppeteer.launch({
         headless: false,
+        executablePath: await chromium.executablePath,
         defaultViewport: {
           width: 1366,
           height: 768,
         },
-        ignoreDefaultArgs: ["--enable-automation"],
+        args: chromium.args,
       })
       await this.requestFirstPage(browser)
     }
@@ -49,8 +43,6 @@ class OOCLService {
     await page.goto(this.MAIN_URL)
 
     this.cookies = await page.cookies()
-
-    await this.parseMainPageParams(page)
 
     await page.$eval('#btn_cookie_accept', btn => btn.click())
     await page.$eval('.btn.dropdown-toggle.btn-default', btn => btn.click())
@@ -65,41 +57,26 @@ class OOCLService {
     await secondPage.waitForSelector("#nc_1_n1z")
     // Got the 2nd page
 
-    // Slide the captcha
+    // Get captcha slider
     const sliderElement = await secondPage.$('#nc_1_n1t')
     const slider = await sliderElement.boundingBox()
 
+    // Get captcha slide button
     const slideThumb = await secondPage.$('#nc_1_n1z')
     const thumb = await slideThumb.boundingBox()
 
+    // Simulate slide captcha slider
     await secondPage.mouse.move(thumb.x + thumb.width / 2, thumb.y + thumb.height / 2)
     await secondPage.mouse.down()
     await secondPage.mouse.move(thumb.x + slider.width, thumb.y + thumb.height / 2, {steps: 10})
     await page.mouse.up()
-    // Slided over the captcha
+    // Done simulate
 
-  }
+    // Close pages and finally close the browser
+    await secondPage.close()
+    await page.close()
+    await browser.close()
 
-  parseMainPageParams = async page => {
-    const inputs = await page.$$('input')
-    for (let input of inputs) {
-      const key = await page.evaluate(el => el.getAttribute("name"), input)
-      if (key === null) continue
-      const value = await page.evaluate(el => el.getAttribute("value"), input)
-      this.params[key] = value
-    }
-    this.params = {...this.params, ...this.defaultMainParams}
-  }
-
-  nextContainer = (containerNo) => {
-    this.currentContainerNo = containerNo
-    this.defaultMainParams = {
-      'searchType': 'cont',
-      'SEARCH_NUMBER': containerNo
-    }
-    this.params = {}
-    this.cookies = null
-    this.userToken = ''
   }
 
   interceptor = async (page, body = {}) => {
